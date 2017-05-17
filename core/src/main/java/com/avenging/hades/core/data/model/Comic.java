4750bdabb901054aef4199272ee2f92dd3f8467f
HadesHe <hezhanghe@126.com>
2017-05-17 08:07:29 +0800

    add image bean

--- core/src/main/java/com/avenging/hades/core/data/model/Comic.java
+++ core/src/main/java/com/avenging/hades/core/data/model/Comic.java
@@ -5,6 +5,8 @@ import android.os.Parcelable;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 /**
  * Created by Hades on 2017/5/17.
  */
@@ -26,4 +28,24 @@ public class Comic implements Parcelable{
     private String mRating;
     @JsonProperty("type")
     private String mType;
+    @JsonProperty("modified")
+    private String mModified;
+    @JsonProperty("name")
+    private String mName;
+    @JsonProperty("resourceURI")
+    private String mResourceUri;
+    @JsonProperty(value = "thumbnail")
+    private Image mThumbnail;
+    @JsonProperty(value = "images")
+    private List<Image> mImageList;
+
+    public Comic(){}
+
+    public String getThumbnailUri(){
+        if(mThumbnail!=null){
+            return mThumbnail.buildCompleteImagePath();
+        }
+
+        return "";
+    }
 }
